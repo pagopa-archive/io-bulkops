4cd77d228e3f062de7e484c2234741a22276a697
Pasquale De Vita <pasqualedevita@Pasquales-MacBook-Pro.local>
2020-06-30 22:43:51 +0200

    added while loop to avoid too many RU

--- src/export-users.ts
+++ src/export-users.ts
@@ -16,6 +16,11 @@ const destination_cosmosdb_container = process.env.DESTINATION_COSMOSDB_CONTAINE
 
 const prefix_partitionKey = process.env.PREFIX_PARTITIONKEY;
 
+const START_DATE = process.env.START_DATE || "";
+const END_DATE = process.env.END_DATE || "";
+const STEP_HOURS = Number(process.env.STEP_HOURS || 24);
+
+
 enum sendStatus {
     NOT_SENDED,
     SENDED
@@ -31,88 +36,104 @@ const newItem = {
 
 async function main() {
 
-    // 15 april 2020 22:00:00 UTC -> 16 april 2020 00:00:00 CEST
-    let fromDate = Date.UTC(2019, 3, 15, 22, 0, 0);
-    // from milliseconds to seconds precision
-    fromDate = fromDate / 1000;
-    // add 2 hour to convert from UTC to CEST
-    fromDate = fromDate + (2 * 60 * 60);
+    const startDate = new Date(START_DATE);
+    const maxDate = new Date(END_DATE);
 
-    // 30 june 2020 21:59:59 UTC -> 30 june 2020 23:59:59 CEST
-    let toDate = Date.UTC(2020, 5, 30, 21, 59, 59);
-    // from milliseconds to seconds precision
-    toDate = toDate / 1000;
-    // add 2 hour to convert from UTC to CEST
-    toDate = toDate + (2 * 60 * 60);
+    let fromDateTs = startDate.getTime() / 1000;
+    let toDateTs = (startDate.getTime() / 1000) + (STEP_HOURS * 60 * 60);
 
     const clientSource = new CosmosClient({ endpoint: source_endpoint, key: source_key });
-
     const databaseSource = clientSource.database(source_cosmosdb_database);
     const containerSource = databaseSource.container(source_cosmosdb_container);
-
-    const querySpec = {
-        query: "SELECT c.fiscalCode, c._ts FROM c WHERE c.version = @version AND c._ts >= @fromDate AND c._ts <= @toDate",
-        parameters: [
-            { name: "@version", value: 0 },
-            { name: "@fromDate", value: fromDate },
-            { name: "@toDate", value: toDate }
-        ]
-    };
-
-    // read all items in the Items container
-    const { resources: items } = await containerSource.items
-        .query(querySpec)
-        .fetchAll();
 
     const clientDestination = new CosmosClient({ endpoint: destination_endpoint, key: destination_key });
     const databaseDestination = clientDestination.database(destination_cosmosdb_database);
     const containerDestination = databaseDestination.container(destination_cosmosdb_container);
 
-    let count = 0;
+    let exitWhile = false;
 
-    for (const item of items) {
+    while (!exitWhile) {
 
-        newItem.fiscalCode = prefix_partitionKey + item.fiscalCode;
-        newItem.onboardingDate = new Date(item._ts * 1000).toISOString();
-        newItem.onboardingTs = item._ts;
+        if (toDateTs > (maxDate.getTime() / 1000)) {
+            // not overlap maxdate
+            exitWhile = true;
+            toDateTs = maxDate.getTime() / 1000;
+        }
 
-        // console.log(newItem.fiscalCode);
+        const querySpec = {
+            query: "SELECT c.fiscalCode, c._ts FROM c WHERE c.version = @version AND c._ts >= @fromDate AND c._ts <= @toDate",
+            parameters: [
+                { name: "@version", value: 0 },
+                { name: "@fromDate", value: fromDateTs },
+                { name: "@toDate", value: toDateTs }
+            ]
+        };
 
-        try {
-            const { resource: createdItem } = await containerDestination.items.create(newItem);
-            // console.log(`\nCreated new item: ${createdItem.fiscalCode}\n`);
-            count = count + 1;
-            console.log(count);
-        } catch (e) {
-            if (e.code != 409) {
-                console.log(e);
+        // read all items in the Items container
+        const { resources: items } = await containerSource.items
+            .query(querySpec)
+            .fetchAll();
+
+        let count = 0;
+
+        for (const item of items) {
+
+            newItem.fiscalCode = prefix_partitionKey + item.fiscalCode;
+            newItem.onboardingDate = new Date(item._ts * 1000).toISOString();
+            newItem.onboardingTs = item._ts;
+
+            // console.log(newItem.fiscalCode);
+
+            try {
+                const { resource: createdItem } = await containerDestination.items.create(newItem);
+                // console.log(`\nCreated new item: ${createdItem.fiscalCode}\n`);
+                count = count + 1;
+                console.log(count);
+            } catch (e) {
+                if (e.code != 409) {
+                    console.log(e);
+                }
             }
+            count = count + 1;
+
+        }
+
+        const queryCountSource = {
+            query: "SELECT VALUE COUNT(1) FROM c WHERE c.version = @version AND c._ts >= @fromDate AND c._ts <= @toDate",
+            parameters: [
+                { name: "@version", value: 0 },
+                { name: "@fromDate", value: fromDateTs },
+                { name: "@toDate", value: toDateTs }
+            ]
+        };
+
+        // read all items in the Items container
+        const { resources: countSource } = await containerSource.items
+            .query(queryCountSource)
+            .fetchAll();
+
+        const queryCountDestination = {
+            query: "SELECT VALUE COUNT(1) FROM c WHERE c.onboardingTs >= @fromDate AND c.onboardingTs <= @toDate",
+            parameters: [
+                { name: "@fromDate", value: fromDateTs },
+                { name: "@toDate", value: toDateTs }
+            ]
+        };
+        const { resources: countDestination } = await containerDestination.items
+            .query(queryCountDestination)
+            .fetchAll();
+
+        console.log(count);
+        console.log('countSource: ' + countSource);
+        console.log('countDestination: ' + countDestination);
+        console.log('last toDateTs: ' + new Date(toDateTs * 1000).toISOString());
+
+        toDateTs = toDateTs + (STEP_HOURS * 60 * 60);
+        if (!exitWhile) {
+            console.log('next toDateTs: ' + new Date(toDateTs * 1000).toISOString());
         }
     }
 
-    const queryCountSource = {
-        query: "SELECT VALUE COUNT(1) FROM c WHERE c.version = @version AND c._ts >= @fromDate AND c._ts <= @toDate",
-        parameters: [
-            { name: "@version", value: 0 },
-            { name: "@fromDate", value: fromDate },
-            { name: "@toDate", value: toDate }
-        ]
-    };
-    // read all items in the Items container
-    const { resources: countSource } = await containerSource.items
-        .query(queryCountSource)
-        .fetchAll();
-
-
-    const queryCountDestination = {
-        query: "SELECT VALUE COUNT(1) FROM c"
-    };
-    const { resources: countDestination } = await containerDestination.items
-        .query(queryCountDestination)
-        .fetchAll();
-
-    console.log('countSource: ' + countSource);
-    console.log('countDestination: ' + countDestination);
     console.log('export users finished');
 
 }
