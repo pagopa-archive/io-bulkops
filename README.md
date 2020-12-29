ea5e4b044a20d24251eb041e0ee29c47be5a510d
pasqualedevita <>
2020-12-29 16:04:53 +0100

    refactor

--- README.md
+++ README.md
@@ -1,45 +1,21 @@
-# TODO
+# IO-BULKMESSAGES
 
-Use this template when creating new typescript projects.
-
-npm install -g ts-node
-npm install -g typescript
-
-npm install
-
-yarn install
-yarn build
-
-cp .env.exsample .env
+### run
 
 ```bash
-# source
-SOURCE_COSMOSDB_KEY="XXX"
-SOURCE_COSMOSDB_ENDPOINT="XXX"
-SOURCE_COSMOSDB_DATABASE="XXX"
-SOURCE_COSMOSDB_CONTAINER="XXX"
+yarn install
+yarn build
+cp .env.exsample .env
+yarn ts-node src/send-messages work/sample.csv work/sample_skip.csv
+```
 
-# destination
-DESTINATION_COSMOSDB_KEY="XXX"
-DESTINATION_COSMOSDB_ENDPOINT="XXX"
-DESTINATION_COSMOSDB_DATABASE="XXX"
-DESTINATION_COSMOSDB_CONTAINER="XXX"
+### sample .env file
 
-PREFIX_PARTITIONKEY="prefix-"
-START_DATE="2020-03-01T00:00:00.000Z"
-END_DATE="2020-04-30T21:59:59.000Z"
-STEP_MINUTES="10"
-MAX_USERS="10"
-
+```bash
 API_URL="https://api.io.italia.it/api/v1/messages"
 SUBSCRIPTION_KEY="XXX"
 SLEEP_MS="100"
 SLEEP429_MS="1000"
-CF_SKIPPED_CSV="src/ignore/file.csv"
+# One of: cashbackIBAN1, cashbackIBAN2, bonusvacanzeStart1
+MESSAGE_CONTENT="XXX"
 ```
-
-### run 
-
-ts-node src/export-users.ts
-ts-node src/send-messages.ts
-
--- src/send-messages.ts
+++ src/send-messages.ts
@@ -1,177 +1,277 @@
-import * as dotenv from "dotenv";
-import fetch from "node-fetch";
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as parsecsv from 'csv-parse/lib/sync';
+import { GetMessageContent } from './messages';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
-const { CosmosClient } = require("@azure/cosmos");
-const fs = require('fs');
-
-const destination_endpoint = process.env.DESTINATION_COSMOSDB_ENDPOINT;
-const destination_key = process.env.DESTINATION_COSMOSDB_KEY;
-const destination_cosmosdb_database = process.env.DESTINATION_COSMOSDB_DATABASE;
-const destination_cosmosdb_container = process.env.DESTINATION_COSMOSDB_CONTAINER;
-
-const prefix_partitionKey = process.env.PREFIX_PARTITIONKEY || "";
-const max_users = Number(process.env.MAX_USERS);
-
-const API_URL = process.env.API_URL || "";
-const subscription_key = process.env.SUBSCRIPTION_KEY || "";
+const apiUrl = process.env.API_URL || '';
+const subscriptionKey = process.env.SUBSCRIPTION_KEY || '';
 const sleep_ms = Number(process.env.SLEEP_MS || 100);
 const sleep429_ms = Number(process.env.SLEEP429_MS || 1000);
-const cf_skipped_csv = process.env.CF_SKIPPED_CSV || "src/ignore/file.csv";
+const messageContent = process.env.MESSAGE_CONTENT || '';
 
-enum sendStatus {
-    NOT_SENDED,
-    SENDED,
-    SKIPPED
-};
+enum SendCSV {
+  fiscalCode = 'fiscalCode',
+}
+
+enum SkipCSV {
+  fiscalCode = 'fiscalCode',
+}
+
+enum SendedCSV {
+  fiscalCode = 'fiscalCode',
+  responseCode = 'responseCode',
+  idMessage = 'idMessage',
+}
 
 async function main() {
+  let cf_send_csv = '';
+  let cf_skip_csv = '';
 
-    const cf_nosend = fs.readFileSync(cf_skipped_csv).toString().replace(/\r\n/g, '\n').split('\n');
+  if (process.argv.length > 2) {
+    cf_send_csv = String(process.argv[2]);
+  } else {
+    console.error('You need to specify input csv');
+    return;
+  }
+  if (process.argv.length > 3) {
+    cf_skip_csv = String(process.argv[3]);
+  }
 
-    const clientDestination = new CosmosClient({ endpoint: destination_endpoint, key: destination_key });
-    const databaseDestination = clientDestination.database(destination_cosmosdb_database);
-    const containerDestination = databaseDestination.container(destination_cosmosdb_container);
+  const logFile = cf_send_csv.replace('.csv', '') + '.log';
+  startLogFile(logFile);
 
-    const querySpec = {
-        query: "SELECT * FROM c WHERE c.sendStatus = @sendStatus OFFSET 0 LIMIT @maxUsers",
-        parameters: [
-            { name: "@sendStatus", value: sendStatus.NOT_SENDED },
-            { name: "@maxUsers", value: max_users }
-        ]
-    };
+  const cf_sended_csv = cf_send_csv.replace('.csv', '') + '_sended.csv';
 
-    // read all items in the Items container
-    const { resources: items } = await containerDestination.items
-        .query(querySpec)
-        .fetchAll();
+  let cf_send_items = undefined;
+  if (fs.existsSync(cf_send_csv)) {
+    // File exists in path
+    const cf_send_file = fs.readFileSync(cf_send_csv).toString();
+    cf_send_items = parsecsv(cf_send_file, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+    console.log('cf_send_items');
+    console.log(cf_send_items);
+  } else {
+    // File doesn't exist in path
+    console.error(cf_send_csv + ' file not found');
+    return;
+  }
 
-    let countProgress = 0;
-    let countTmpProgress = 0;
-    let countSkipped = 0;
-    let countSended = 0;
-    let countNotSended = 0;
-    let countNotSended429 = 0;
+  let cf_sended_items = undefined;
 
-    for (const item of items) {
+  if (fs.existsSync(cf_sended_csv)) {
+    // File exists in path
+    const cf_sended_file = fs.readFileSync(cf_sended_csv).toString();
+    cf_sended_items = parsecsv(cf_sended_file, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+    console.log('cf_sended_items');
+    console.log(cf_sended_items);
+  } else {
+    // File doesn't exist in path
+  }
 
-        try {
+  let cf_skip_items = undefined;
 
-            const fiscalCodePure = String(item.fiscalCode).replace(prefix_partitionKey, "");
+  if (fs.existsSync(cf_skip_csv)) {
+    // File exists in path
+    const cf_skip_file = fs.readFileSync(cf_skip_csv).toString();
+    cf_skip_items = parsecsv(cf_skip_file, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+    console.log('cf_skip_csv');
+    console.log(cf_skip_csv);
+  } else {
+    // File doesn't exist in path
+  }
 
-            if (cf_nosend.indexOf(fiscalCodePure) != -1) {
-
-                item.sendStatus = sendStatus.SKIPPED;
-
-                const { id, fiscalCode } = item;
-                const { resource: updatedItem } = await containerDestination
-                    .item(id, fiscalCode)
-                    .replace(item);
-
-                console.log(`1 user skipped\n`);
-
-                countSkipped = countSkipped + 1;
-
-            } else {
-
-                const rawResponse = await submitMessageforUser(fiscalCodePure, subscription_key);
-                const content = await rawResponse.json();
-
-                if (rawResponse.status == 201) {
-
-                    item.sendStatus = sendStatus.SENDED;
-                    item.idMessage = content.id;
-                    item.httpStatusCode = rawResponse.status;
-
-                    const { id, fiscalCode } = item;
-                    const { resource: updatedItem } = await containerDestination
-                        .item(id, fiscalCode)
-                        .replace(item);
-
-                    countSended = countSended + 1;
-
-                } else {
-
-                    item.httpStatusCode = rawResponse.status;
-
-                    const { id, fiscalCode } = item;
-                    const { resource: updatedItem } = await containerDestination
-                        .item(id, fiscalCode)
-                        .replace(item);
-
-                    console.error(`Error httpStatusCode to ${updatedItem.httpStatusCode}\n`);
-
-                    countNotSended = countNotSended + 1;
-
-                    if (rawResponse.status == 429) {
-                        await delay(sleep429_ms);
-                        countNotSended429 = countNotSended429 + 1;
-                    }
-
-                }
-
-                countTmpProgress = countTmpProgress + 1;
-                countProgress = countProgress + 1;
-
-                if (countTmpProgress >= 100) {
-                    console.log('sending....: ' + countProgress);
-                    countTmpProgress = 0;
-                }
-
-                await delay(sleep_ms);
-
-            }
-
-        } catch (e) {
-            console.error(e);
-        }
-
+  if (cf_skip_items != undefined) {
+    for (const cf_skip of cf_skip_items) {
+      cf_send_items = cf_send_items.filter(
+        (x: { [x: string]: any }) =>
+          x[SendCSV.fiscalCode] != cf_skip[SkipCSV.fiscalCode]
+      );
     }
+  }
 
-    console.log('countProcessed: ' + countProgress);
-    console.log('countSkipped ok: ' + countSkipped);
-    console.log('countSended ok: ' + countSended);
-    console.log('countNotSended ko: ' + countNotSended);
-    console.log('countNotSended429 ko: ' + countNotSended429);
+  if (cf_sended_items != undefined) {
+    const cf_sended_skip_items = cf_sended_items.filter(
+      (x: { [x: string]: string }) => x[SendedCSV.responseCode] == '201'
+    );
+    for (const cf_sended_skip of cf_sended_skip_items) {
+      cf_send_items = cf_send_items.filter(
+        (x: { [x: string]: any }) =>
+          x[SendCSV.fiscalCode] != cf_sended_skip[SendedCSV.fiscalCode]
+      );
+    }
+  }
 
-    console.log('send message finished');
+  console.log('cf_send_items clean');
+  console.log(cf_send_items);
 
+  // create sending log items
+  const cf_sendeding_csv = cf_send_csv.replace('.csv', '') + '_sendeding.csv';
+  fs.writeFileSync(
+    cf_sendeding_csv,
+    SendedCSV.fiscalCode +
+      ',' +
+      SendedCSV.responseCode +
+      ',' +
+      SendedCSV.idMessage +
+      '\n'
+  );
+
+  // backup existing sended messages
+  if (cf_sended_items != undefined) {
+    for (const cf of cf_sended_items.filter(
+      (x: { [x: string]: string }) => x[SendedCSV.responseCode] == '201'
+    )) {
+      fs.writeFileSync(
+        cf_sendeding_csv,
+        cf.fiscalCode + ',' + cf.responseCode + ',' + cf.idMessage + '\n',
+        { flag: 'a+' }
+      );
+    }
+  }
+
+  appendLogFile(logFile, 'Info - message to send: ' + cf_send_items.length);
+
+  // send messages
+  for (const cf of cf_send_items) {
+    try {
+      const rawResponse = await submitMessageforUser(
+        cf[SendCSV.fiscalCode],
+        subscriptionKey
+      );
+      const content = await rawResponse.json();
+
+      let newSendItem = {
+        fiscalCode: cf[SendCSV.fiscalCode],
+        responseCode: rawResponse.status,
+        idMessage: '',
+      };
+
+      if (rawResponse.status == 201) {
+        newSendItem.idMessage = content.id;
+      } else {
+        console.error(
+          `Error - fiscalCode: ${cf[SendCSV.fiscalCode]} httpStatusCode: ${
+            rawResponse.status
+          }\n`
+        );
+        appendLogFile(
+          logFile,
+          `Error - fiscalCode: ${cf[SendCSV.fiscalCode]} httpStatusCode: ${
+            rawResponse.status
+          }`
+        );
+        if (rawResponse.status == 429) {
+          await delay(sleep429_ms);
+          const rawResponseRetry = await submitMessageforUser(
+            cf[SendCSV.fiscalCode],
+            subscriptionKey
+          );
+          const contentRetry = await rawResponseRetry.json();
+          newSendItem.responseCode = rawResponseRetry.status;
+          if (rawResponseRetry.status == 201) {
+            newSendItem.idMessage = contentRetry.id;
+            appendLogFile(logFile, `Error - 429 resolved`);
+          }
+        }
+      }
+
+      // save sended message
+      fs.writeFileSync(
+        cf_sendeding_csv,
+        newSendItem.fiscalCode +
+          ',' +
+          newSendItem.responseCode +
+          ',' +
+          newSendItem.idMessage +
+          '\n',
+        { flag: 'a+' }
+      );
+
+      await delay(sleep_ms);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  let nowDate = new Date();
+  const cf_sended_old_csv =
+    cf_send_csv.replace('.csv', '') +
+    '_sended_' +
+    nowDate.getFullYear() +
+    nowDate.getMonth() +
+    nowDate.getDay() +
+    nowDate.getHours() +
+    nowDate.getMinutes() +
+    nowDate.getSeconds() +
+    '.csv';
+
+  if (fs.existsSync(cf_sended_csv)) {
+    fs.copyFileSync(cf_sended_csv, cf_sended_old_csv);
+    fs.unlinkSync(cf_sended_csv);
+  }
+
+  fs.copyFileSync(cf_sendeding_csv, cf_sended_csv);
+  fs.unlinkSync(cf_sendeding_csv);
+
+  endLogFile(logFile);
 }
 
 main().catch((error) => {
-    console.error(error);
+  console.error(error);
 });
 
 function delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const MESSAGE_CONTENT = {
-    subject: "E' arrivato il Bonus Vacanze!",
-    markdown:
-        "---\nit:\n    cta_1: \n        text: \"Richiedi il Bonus Vacanze\"\n        action: \"ioit://BONUS_AVAILABLE_LIST\"\nen:\n    cta_1: \n        text: \"Claim the Bonus Vacanze\"\n        action: \"ioit://BONUS_AVAILABLE_LIST\"\n---\n\n\nDal 1 luglio puoi richiedere il **Bonus Vacanze**, istituito dal Decreto Rilancio per incentivare il turismo dopo il lockdown dovuto all'emergenza Coronavirus.\n\nIl bonus può valere **fino a 500 euro**, a seconda della numerosità del nucleo familiare, ed è spendibile per **soggiorni in Italia**, presso imprese turistiche ricettive, agriturismi e bed & breakfast, **dal 1 luglio al 31 dicembre 2020**.\n\n**Possono ottenere il contributo i nuclei familiari con ISEE fino a 40.000 euro.**\n\nSe non l’hai ancora chiesto o attivato e sei maggiorenne, scopri come funziona e richiedilo adesso.\n\nPer poter richiedere il Bonus Vacanze, devi avere aggiornato IO all'ultima versione disponibile.\n\n[App Store](https://apps.apple.com/it/app/io/id1501681835)\n\n[Play Store](https://play.google.com/store/apps/details?id=it.pagopa.io.app)\n\n"
-};
 // Submit the message to a user identified by his/her fiscal code.
 async function submitMessageforUser(
-    fiscal_code: string,
-    subscription_key: string
+  fiscal_code: string,
+  subscription_key: string
 ) {
-    let message = {
-        content: MESSAGE_CONTENT,
-        time_to_live: 3600,
-        fiscal_code: fiscal_code,
-    };
-    const rawResponse = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            "Ocp-Apim-Subscription-Key": subscription_key,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(message),
-    });
-    //const content = await rawResponse.json();
-    //console.log(content);
-    //console.log(rawResponse.status);
-    return rawResponse;
+  let message = {
+    content: GetMessageContent(messageContent),
+    time_to_live: 3600,
+    fiscal_code: fiscal_code,
+  };
+  const rawResponse = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Ocp-Apim-Subscription-Key': subscription_key,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+  //const content = await rawResponse.json();
+  //console.log(content);
+  //console.log(rawResponse.status);
+  return rawResponse;
+}
+
+function startLogFile(filepath: string) {
+  const nowDate = new Date().toISOString();
+  fs.writeFileSync(filepath, nowDate + ' ---- START ----\n');
+}
+
+function appendLogFile(filepath: string, content: string) {
+  const nowDate = new Date().toISOString();
+  fs.writeFileSync(filepath, nowDate + ' ---- ' + content + '\n', {
+    flag: 'a+',
+  });
+}
+
+function endLogFile(filepath: string) {
+  const nowDate = new Date().toISOString();
+  fs.writeFileSync(filepath, nowDate + ' ---- END ----\n', { flag: 'a+' });
 }
